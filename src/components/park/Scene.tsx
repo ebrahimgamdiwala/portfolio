551a3cc18bb24e7b9f2bb892fe5d762f00e07fed
ebrahimgamdiwala <ebrahimgamdiwala@gmail.com>
2026-08-06 22:24:17 +0530

    Revert Preload all to fix frozen loading screen

--- src/components/park/Scene.tsx
+++ src/components/park/Scene.tsx
@@ -28,7 +28,6 @@ import { Funfair } from "./Funfair";
 import { Midway } from "./Midway";
 import { Structures } from "./Structures";
 import { Props } from "./Props";
-import { Preload } from "@react-three/drei";
 import { Atmosphere } from "./Atmosphere";
 import { Fireworks } from "./Fireworks";
 import { LightPool } from "./LightPool";
@@ -171,9 +170,7 @@ export function Scene({ world }: { world: ParkWorld }) {
 
       <Atmosphere />
       <Fireworks />
-
       <Effects />
-      <Preload all />
     </ParkProvider>
   );
 }
